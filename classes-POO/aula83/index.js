69decbb2ba6bff36ad5edfbd50d7bf84d26f817d
Carlos Flavio <carloslflavio@gmail.com>
2024-01-31 18:59:26 -0300

    Comitando as aulas de orientação a objetos

---
+++ classes-POO/aula83/index.js
@@ -0,0 +1,15 @@
+class ControleRemeto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+
+    aumentarVolume() {
+        this.volume += 2;
+    }
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+}
+
+const controle1 = new ControleRemeto
---
+++ classes-POO/aula85/assets/js/main.js
@@ -0,0 +1,5 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario')
+    }
+}
